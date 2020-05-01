ebe78f95bc6daa34f17971d18498b6034c85b61f
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-05-01 16:45:33 +0300

    docs: longest common subsequence

--- Assignments/cs110_assignment_6/helpers.py
+++ Assignments/cs110_assignment_6/helpers.py
@@ -1,4 +1,5 @@
 import mmh3
+import numpy as np
 
 
 def rabin_karp_matcher(target, potential, d, q):
@@ -111,3 +112,34 @@ def regular_get_match(x, y, k):
             output.append((i, match))
 
     return output
+
+
+def longest_common_subsequence(x, y):
+    """
+    Gives the length of the longest common substring between strings x and y
+
+    :param x: The first string for lcs generation
+    :param y: The second string for lcs generation
+    """
+
+    # find the length of the strings
+    m = len(x)
+    n = len(y)
+
+    # declaring the array for storing the dp values
+    lcs = np.zeros((m + 1, n + 1))
+
+    # iterate through each sub problem
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                lcs[i, j] = 0
+            elif x[i - 1] == y[j - 1]:
+                lcs[i, j] = lcs[i - 1, j - 1] + 1
+            else:
+                # use the optimal substructure property
+                # of using already computed results previous subproblems
+                lcs[i, j] = max(lcs[i - 1, j], lcs[i, j - 1])
+
+    # L[m,n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
+    return lcs[m, n]
